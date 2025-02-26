d778f08de50327b33a04b6d132f2acfb55c01356
Bakops <bakoutoure4@gmail.com>
2025-02-26 14:24:23 +0100

    ESCAPE DONE

--- src/main/java/org/sebsy/demo/escaperooms/controller/EscapeRoom1Controller.java
+++ src/main/java/org/sebsy/demo/escaperooms/controller/EscapeRoom1Controller.java
@@ -1,5 +1,10 @@
 package org.sebsy.demo.escaperooms.controller;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
 public class EscapeRoom1Controller {
 
     public void entreeSalle1() {
--- src/main/java/org/sebsy/demo/escaperooms/controller/EscapeRoom2Controller.java
+++ src/main/java/org/sebsy/demo/escaperooms/controller/EscapeRoom2Controller.java
@@ -1,11 +1,19 @@
 package org.sebsy.demo.escaperooms.controller;
 
 import org.sebsy.demo.escaperooms.bll.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+@Component("room2")
 public class EscapeRoom2Controller {
+
     private RoomService roomService;
 
-    public EscapeRoom2Controller(RoomService roomService) {
+    @Autowired
+    public EscapeRoom2Controller( RoomService roomService) {
         this.roomService = roomService;
     }
 
